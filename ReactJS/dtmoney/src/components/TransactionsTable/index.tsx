4623906c931ae95418dd448bf02037de2c23f680
MarcosOliveir4 <marcosassis071@gmail.com>
2022-05-01 13:32:27 -0300

    :lipstick: componentizando a transactionsTable

---
+++ ReactJS/dtmoney/src/components/TransactionsTable/index.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Container } from "./styles";
+
+export const TransactionsTable: React.FC = () => {
+  return (
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Título</th>
+            <th>Valor</th>
+            <th>Categoria</th>
+            <th>Data</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>Desenvolvimento de website</td>
+            <td className="deposit">R$12.000</td>
+            <td>Desenvolvimento</td>
+            <td>01/05/2022</td>
+          </tr>
+
+          <tr>
+            <td>Aluguel</td>
+            <td className="withdraw">- R$1.100</td>
+            <td>Casa</td>
+            <td>30/04/2022</td>
+          </tr>
+        </tbody>
+      </table>
+    </Container>
+  );
+};
