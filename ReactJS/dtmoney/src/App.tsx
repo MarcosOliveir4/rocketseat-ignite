f6c14c62e1fcdfb551b6aab040ac2007d61533db
MarcosOliveir4 <marcosassis071@gmail.com>
2022-05-01 11:27:54 -0300

    :art: criando estilos globais

--- ReactJS/dtmoney/src/App.tsx
+++ ReactJS/dtmoney/src/App.tsx
@@ -1,3 +1,10 @@
+import { GlobalStyle } from "./styles/global";
+
 export function App() {
-  return <h1>dtmoney</h1>;
+  return (
+    <>
+      <h1>dtmoney</h1>
+      <GlobalStyle />
+    </>
+  );
 }
