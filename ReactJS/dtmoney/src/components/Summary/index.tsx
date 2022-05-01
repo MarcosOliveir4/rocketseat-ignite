9f5b498cb466c4b06c4930680f53dac2384c6c8a
MarcosOliveir4 <marcosassis071@gmail.com>
2022-05-01 12:33:03 -0300

    :lipstick: componentizando o summary

---
+++ ReactJS/dtmoney/src/components/Summary/index.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+import entradasImg from "../../assets/entradas.svg";
+import saidasImg from "../../assets/saidas.svg";
+import totalImg from "../../assets/total.svg";
+
+import { Container } from "./styles";
+
+export const Summary: React.FC = () => {
+  return (
+    <Container>
+      <div>
+        <header>
+          <p>Entradas</p>
+          <img src={entradasImg} alt="Entradas" />
+        </header>
+        <strong>R$1000,00</strong>
+      </div>
+
+      <div>
+        <header>
+          <p>Saídas</p>
+          <img src={saidasImg} alt="Saídas" />
+        </header>
+        <strong>-R$500,00</strong>
+      </div>
+
+      <div className="highlight-background">
+        <header>
+          <p>Total</p>
+          <img src={totalImg} alt="Total" />
+        </header>
+        <strong>R$500,00</strong>
+      </div>
+    </Container>
+  )
+};
